60eefc410eb4ffbedd2096377301f4184fa34a5f
Naofel Badsha <naofelbadsha@gmail.com>
2026-04-18 01:34:35 +0600

    added client side

---
+++ book-management-app-client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="">
+      <h1> Hello Developers</h1>
+      <h2>How are you</h2>
+    </div>
+  );
+}
+
+export default App;
