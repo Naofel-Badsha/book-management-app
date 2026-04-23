32006cb1beedcfca491b2470ca9f9310bb131dd1
Naofel-Badsha <naofelbadsha@gmail.com>
2026-04-24 03:02:16 +0600

    added bata by batabase

--- book-management-app-client/src/Context/BookContext.jsx
+++ book-management-app-client/src/Context/BookContext.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 
 const BookContext = createContext();
 
@@ -9,20 +9,56 @@ export const BookProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchBooks = async () => {
+  //----------To banckend----------
+  const [filter, setFilter] = useState({
+    page: 1,
+    limit: 8,
+    genre: '',
+    minYear: '',
+    maxYear: '',
+    author: '',
+    minPrice: '',
+    maxPrice: '',
+    sortBy: 'title',
+    order: 'asc',
+    search: ''
+  })
+
+  //------------Pagination------------
+  const [pagination, setPagination] = useState({
+    totalBooks: 0,
+    currentPage: 1,
+    totalPages: 1
+  })
+
+
+  const fetchBooks = useCallback(async () => {
     try {
       setLoading(true);
       setError(null);
+
+      const params = new URLSearchParams();
+      Object.entries(filter).forEach(([key, value]) => {
+        if (value !== '') {
+          params.append(key, value)
+        }
+      })
+
       const response = await axios.get("http://localhost:3000/books");
       setBooks(response.data.books);
     } catch (error) {
       console.log(error);
     }
-  };
+  })
+
+
+
 
   useEffect(() => {
     fetchBooks();
   }, []);
+
+
 
   const value = {
     books,
