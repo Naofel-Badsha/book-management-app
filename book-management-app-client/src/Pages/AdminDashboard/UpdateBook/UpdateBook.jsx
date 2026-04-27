be827c0a32ea96703fa03f04d2f6b0889a522941
Naofel-Badsha <naofelbadsha@gmail.com>
2026-04-27 21:06:19 +0600

    added update book form

--- book-management-app-client/src/Pages/AdminDashboard/UpdateBook/UpdateBook.jsx
+++ book-management-app-client/src/Pages/AdminDashboard/UpdateBook/UpdateBook.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const UpdateBook = () => {
   return (
-    <div>
-      <h1>UpdateBook</h1>
+<div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Update Product</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
     </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <fieldset className="fieldset">
+          <label className="label">Book Title</label>
+          <input type="text" name="Book Title" className="input" placeholder="Book Title" />
+
+          <label className="label">Book Price</label>
+          <input type="text" name="Book Price" className="input" placeholder="Book Price" />
+
+          <label className="label">Book Details</label>
+          <input type="text" name="Book Details" className="input" placeholder="Book Details" />
+
+          <label className="label">Author</label>
+          <input type="text" name="Book Details" className="input" placeholder="Author" />
+
+          <label className="label">Category</label>
+          <input type="text" className="input" placeholder="Category" />
+
+          <button className="btn btn-neutral mt-4">Update Book</button>
+        </fieldset>
+      </div>
+    </div>
+  </div>
+</div>
   )
 }
 
